578062cc176bf43256d458b0db98f3764806fb81
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-02-12 09:06:35 -0400

    Proyecto creado

---
+++ src/test/java/com/example/convertirDivisas_Java/ConvertirDivisasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertirDivisas_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertirDivisasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
